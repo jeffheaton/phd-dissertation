e3e2fbdfe0192ae685559809711c1fcd46668ea7
Jeff Heaton <jeff@jeffheaton.com>
2016-06-07 06:00:12 -0500

    parse model types

---
+++ gp_java/src/main/java/com/jeffheaton/dissertation/experiments/manager/ParseModelType.java
@@ -0,0 +1,37 @@
+package com.jeffheaton.dissertation.experiments.manager;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by jeff on 6/7/16.
+ */
+public class ParseModelType {
+
+    private final String name;
+    private final String type;
+    private final String target;
+
+    public ParseModelType(String str) {
+        String regexp = "([A-Za-z]+)-+([A-Za-z]+):+([A-Za-z]+)";
+
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(str);
+        matcher.find();
+        this.name = matcher.group(1);
+        this.type = matcher.group(2);
+        this.target = matcher.group(3);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+}
